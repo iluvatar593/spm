2c7074e0570b2237121a3b7a0f2b76ba4c6c2cfe
Alessandro <aless.lenzi@gmail.com>
2014-11-06 15:02:15 +0100

    Working Farm for Intel Xeon Phi using intrinsics

---
+++ FarmIntrinsicDouble/src/utils.hpp
@@ -0,0 +1,125 @@
+/*
+ * utils.hpp
+ *
+ *  Created on: 05/nov/2014
+ *      Author: alessandro
+ */
+
+#ifndef UTILS_HPP_
+#define UTILS_HPP_
+
+#include <stdio.h>
+#include <stdlib.h>
+#define start_time() \
+	auto start = std::chrono::high_resolution_clock::now();
+/** Shows the elapsed time. See start_time for usage*/
+#define elapsed_time(STRING) \
+	auto elapsed = std::chrono::high_resolution_clock::now() - start; \
+	long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count(); \
+	printf(#STRING":%lld\n", microseconds);
+#define SPARE_MEMORY 3*1024*1024*1024 //6 gb
+
+#ifndef TILE
+	#define TILE 120
+#endif
+#ifndef ALIGNMENT
+	#define ALIGNMENT 64
+#endif
+#define _MM_MALLOC(dst, type, size) \
+	{(dst) = (type) _mm_malloc((size), ALIGNMENT); \
+		if((dst) == nullptr) { \
+			printf("ERROR: couldn't allocate memory \n"); \
+			exit(-1);			\
+		} \
+	}
+#define _MM_FREE(dst) {_mm_free(dst);}
+
+/**
+* Maximum size of the input buffer, in terms of the number of couples of matrix that can be allocated in it.
+* Parameters:
+* 	s: dimension of the representation of the data type
+*	n: rows of the first matrix
+*	k : cols of the first matrix / rows of the second one
+*	m : cols of the second matrix
+*	numWorkers : number of workers desired (each one has its own matrix)
+*	streamLength : length of the stream
+*
+*	returns: number of input couples that can be allocated.
+ * */
+inline int calculateBufferSize(size_t s, int n, int k, int m, int numWorkers, int streamLength, bool tryanyway) {
+	long outputSize = (long) m*k*s*numWorkers;
+	if(outputSize > ((long)SPARE_MEMORY)) {
+		if(tryanyway){
+			if(isatty(fileno(stdout))) printf("I will go with 6 matrices, but a SEGFAULT is likely\n");
+			return 0;
+		}
+		if(isatty(fileno(stdout))) printf("Sorry, but it is not possible to allocate a %d x %d matrix for every worker, thus the job cannot start. \n Please try again decreasing the number of workers or the dimensions.\n", n, m);
+		exit(-1);
+	}
+	long remainingSize = ((long) SPARE_MEMORY) - outputSize;
+	long coupleSize = s*(n*k+k*m);
+	long totalNumber = remainingSize/coupleSize;
+	if (totalNumber > streamLength) return streamLength;
+	return (int) totalNumber;
+}
+/**
+ * Transpose tha matrix a into aT.
+ * Parameters are:
+ * a : input matrix
+ * aT : output matrix
+ * n : number of rows to transpose
+ * k : number of columns to transpose
+ * lda : number of columns in the matrix a
+ * ldaT : number of columns in the matrix aT
+ *
+ * modifies aT
+ * requires ldaT > k
+ */
+template<typename NUM>
+inline void recTranspose(NUM *__restrict__ a, NUM *__restrict__ aT, const int n, const int k, const int lda, const int ldat){
+	if (n*k <= 512) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < k; j++) {
+				aT[j*ldat+i] = a[i*lda+j];
+			}
+		}
+		return;
+	}
+	if(k > n) {
+		recTranspose(a, aT, n, (k+1)/2, lda, ldat);
+		recTranspose(&a[(k+1)/2], &aT[(k+1)/2*ldat], n, k-((k+1)/2), lda, ldat);
+	} else {
+		recTranspose(a, aT, (n+1)/2, k, lda, ldat);
+		recTranspose(&a[(n+1)/2*lda], &aT[(n+1)/2], n- (n+1)/2, k, lda, ldat);
+	}
+}
+/**
+ * Shows usage
+ */
+void printUsage() {
+	printf("Usage: ./FarmIntrinsicDouble.o mxw numWorkers n k m [tryanyway]\n");
+	printf("Where:\n");
+	printf("mxw is the number of matrices for each worker\n");
+	printf("numWorkers is the number of threads allocated to the stream\n");
+	printf("n k and m are the three dimensions of the matrix \n");
+	printf("Optional tryanyway can be used (if equal to 1) to start the computation even at risk of insufficient memory\n");
+}
+
+/** (Random) initialization of double matrix "matrix" with rows rows and columns cols.
+ *
+ */
+void initializeDoubleMatrix(double *matrix, int rows, int cols) {
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < cols; j++) {
+			matrix[i*cols+j] = ((double) (rand()%50)/(rand()%20+1.1));
+		}
+	}
+}
+
+typedef struct __attribute__((align(ALIGNMENT))) {
+	double *__restrict__ a;
+	double *__restrict__ b;
+	int num;
+} taskDouble_t;
+
+#endif /* UTILS_HPP_ */
---
+++ FarmIntrinsicDouble/src/worker_mic.hpp
@@ -0,0 +1,49 @@
+/*
+ * worker_mic.hpp
+ *
+ *  Created on: 05/nov/2014
+ *      Author: alessandro
+ */
+
+#ifndef WORKER_MIC_HPP_
+#define WORKER_MIC_HPP_
+
+#include <ff/node.hpp>
+#include "matrixmultiplication_mic.hpp"
+#include "utils.hpp"
+
+using namespace ff;
+
+class WorkerDoubleMic : public ff_node {
+public:
+	WorkerDoubleMic(int n, int oldn, int k, int oldk, int m, int oldm):
+		n(n), oldn(oldn), k(k), oldk(oldk), m(m), oldm(oldm){
+		_MM_MALLOC(C, double*, sizeof(double)*n*m);
+	}
+	~WorkerDoubleMic() {_MM_FREE(C);}
+	void *svc(void *__restrict__ task) {
+
+		taskDouble_t *t = (taskDouble_t *) task;
+		double *__restrict__ a = t->a;
+		double *__restrict__ b = t->b;
+
+		double aT[k*30] __attribute__ ((aligned(64)));
+
+		for(int i = 0; i < oldn; i+=30) {
+			recTranspose<double>(&a[i*k], aT, 30, k, k, 30);
+			for(int j = 0; j < oldm; j+=8) {
+				MMKernel(aT, &b[j], &C[i*m+j], m, k/TILE);
+			}
+			/** n/2 can always be divided by 30*/
+			//if(i >= n/2) ff_send_out(C);
+		}
+
+		return (oldn > n/2) ? &C[n/2] : C;
+	}
+private:
+	int n, oldn, k, oldk, m, oldm;
+	double *__restrict__ C;
+};
+
+
+#endif /* WORKER_MIC_HPP_ */
